50682c9c5eda2eefd3bdb9c1fc5a690196253964
Nora Chaynane <93669746+norachaynane@users.noreply.github.com>
2022-07-25 00:55:18 -0500

    Add files via upload

--- js/decoratemytext.js
+++ js/decoratemytext.js
@@ -1,71 +1,169 @@
-//window.onload = function() {
-   // "use strict";
-    function hello() {
-        alert("Hello, World!");
-    }
+window.onload = function() {
 
-    function interval() {
-        var id = setInterval(enlargeText, 1000);
-        setTimeout(clearInterval, 5000, id);
-    }
+    document.getElementById("btnBold").onclick = timerDecorator;
+    document.getElementById("checkboxBling").addEventListener('change', e => {
 
-    function enlargeText() {
-        // document.getElementById("textarea").style.fontSize="24pt";
-        var prop = window.getComputedStyle(document.getElementById("textarea")).fontSize;
-        newProp = parseInt(prop) + 2 + "pt";
-        document.getElementById("textarea").style.fontSize = newProp;
-    }
+        if (e.target.checked) {
+            document.getElementById("txtarea").style.fontWeight = "bold"
+            document.getElementById("txtarea").style.color = "green";
+            document.getElementById("txtarea").style.textDecoration = "underline";
+            document.body.style.backgroundImage = "url('./images/background.png')";
 
-    function boldText() {
-        if (document.getElementById("bling").checked == true) {
-            document.getElementById("textarea").style.fontWeight = "bold";
-            document.getElementById("textarea").style.color = "red";
-            document.getElementById("textarea").style.textDecoration = "underline";
-            document.body.style.backgroundImage = 'url("http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg")';
+
         } else {
-            document.getElementById("textarea").style.fontWeight = "";
-            document.getElementById("textarea").style.color = "";
-            document.getElementById("textarea").style.textDecoration = "";
-            document.body.style.backgroundImage = "";
+            document.getElementById("txtarea").style.fontWeight = "normal"
+            document.getElementById("txtarea").style.color = "red";
+            document.getElementById("txtarea").style.textDecoration = "none";
+            document.body.style.backgroundImage = "none";
 
         }
+
+    });
+
+    document.getElementById("igpay").onclick = igpay;
+    document.getElementById("malkovitch").onclick = checkWord;
+
+};
+
+
+
+function toggleBling() {
+    var isChecked = $('#checkboxBling').attr('checked') ? true : false
+    if (isChecked) {
+        alert("yes")
+    } else {
+        alert("noo")
+
+    }
+}
+
+function timerDecorator() {
+    var timer = null;
+    if (timer === null) {
+        timer = setInterval(changeStyle, 500);
+    } else {
+        x++;
+        timer = setInterval(changeStyle, 500);
+        if (x == 5) {
+            window.clearInterval(timer);
+        }
+    }
+}
+
+function changeStyle() {
+    document.getElementById("txtarea").style.color = "red";
+    //  document.getElementById("txtarea").style.fontSize = "24pt";
+
+    var currentFont = document.getElementById("txtarea");
+    var style = window.getComputedStyle(currentFont, null).getPropertyValue('font-size');
+    var fontSize = parseFloat(style);
+    // now you have a proper float for the font size (yes, it can be a float, not just an integer)
+    currentFont.style.fontSize = (fontSize + 2) + 'pt';
+
+}
+
+
+function vowelTest(s) {
+    return (/^[aeiou]$/i).test(s);
+}
+
+function appendAy(a) {
+    if (checkSpace(a) === false) {
+        a = a + "ay";
+        return a;
+    }
+}
+
+function checkSpace(s) {
+    return (/^\s+$/).test(s);
+}
+
+function checkWord() {
+    var cal = document.getElementById('txtarea');
+    var sentence = cal.value.trim();
+    // var sen=([^\s-]).sentence;
+    var nextLine = sentence.split("\n");
+    var result = [nextLine.length];
+    for (var line = 0; line < nextLine.length; line++) {
+
+        var a = nextLine[line].split(" ");
+        for (var i = 0; i < a.length; i++) {
+            if (a[i].length >= 5) {
+                a[i] = "Malkovich";
+            }
+
+
+        }
+
+        var eachSentence = "";
+        for (var s = 0; s < a.length; s++) {
+
+            if (!checkSpace(a[s])) {
+                eachSentence = eachSentence + a[s] + " ";
+
+            }
+
+        }
+
+        result[line] = eachSentence;
+
+    }
+    var arrayFinal = "";
+
+    for (var res = 0; res < result.length; res++) {
+        arrayFinal = arrayFinal + result[res] + "\n";
     }
 
-    function pig() {
-        var words = document.getElementById("textarea").value;
-        var splittedword = words.split(/\s+/);
-        var result = "";
-        for (var i = 0; i < splittedword.length; i++) {
-            if (splittedword[i].charAt(0).match("[aeoiuAEOIU]")) {
-                changed = splittedword[i] + "ay";
+    cal.value = arrayFinal;
+
+
+}
+
+function character(a) {
+    if (a.length > 5) {
+        return "Malkovich";
+    }
+}
+
+function igpay() {
+    var cal = document.getElementById('txtarea');
+    var sentence = cal.value.trim();
+
+    var nextLine = sentence.split("\n");
+    var result = [nextLine.length];
+    for (var line = 0; line < nextLine.length; line++) {
+
+        var a = nextLine[line].split(" ");
+        for (var i = 0; i < a.length; i++) {
+            var firstLetter = a[i].charAt(0);
+            var test = vowelTest(firstLetter);
+            if (test === false) {
+                a[i] = a[i].substring(1, a[i].length) + firstLetter + "ay";
+
             } else {
-                var word = splittedword[i];
-                for (var j = 0; j < word.length; j++) {
-                    if (word[j].match("[aeoiuAEOIU]")) {
-                        var changed = word.substring(j) + word.substring(0, j) + "ay";
-                        break;
-                    }
 
-                }
+                a[i] = a[i] + "ay";
             }
-            result += changed + " ";
+        }
+
+        var eachSentence = "";
+        for (var s = 0; s < a.length; s++) {
+
+            if (!checkSpace(a[s])) {
+                eachSentence = eachSentence + a[s] + " ";
+
+            }
 
         }
-        document.getElementById("textarea").value = result;
+
+        result[line] = eachSentence;
 
     }
+    var arrayFinal = "";
 
-    function markov() {
-        var words = document.getElementById("textarea").value;
-        var arr = words.split(/\s+/);
-        var result = "";
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i].length >= 5) {
-                arr[i] = "Malkovich";
-            }
-            result += arr[i] + " ";
-        }
-        document.getElementById("textarea").value = result;
+    for (var res = 0; res < result.length; res++) {
+        arrayFinal = arrayFinal + result[res] + "\n";
     }
-//};
 
+    cal.value = arrayFinal;
+}
